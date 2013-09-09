84ef78cc24d3c500382b685a9c69493fcccb5e3a
yochanan.miykael <yochanan.miykael@gmail.com>
2013-09-09 23:10:53 +0300

    added As-Admin function

---
+++ Modules/AsAdmin/AsAdmin.psm1
@@ -0,0 +1,14 @@
+﻿function As-Admin {
+    $prg = $args[0]
+    $argList = New-Object 'System.Collections.Generic.List[System.Object]'
+    
+    for ($i=1; $i -lt $args.Count; $i++) {
+        $argList.Add($args[$i])
+    }
+    if($argList.Count -eq 0) {
+        start $prg -Verb runas
+    } else {
+        start $prg -ArgumentList $argList -Verb runas
+    }
+}
+Export-ModuleMember As-Admin
