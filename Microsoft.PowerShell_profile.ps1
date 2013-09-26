5919a70ccc3a7c269d0ced33e36a50aaad1f9fb4
yochanan.miykael <yochanan.miykael@gmail.com>
2013-09-26 11:52:58 +0300

    added notepad alias

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
 ﻿$profilepath = Split-Path $PROFILE -parent
 powershell -NoProfile -Command "cd $profilepath; git pull"
+$notepad  = "C:\Program Files (x86)\Notepad++\notepad++.exe"
 $devenv   = "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe"
 $devenv10 = "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe"
 $devenv11 = "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe"
@@ -8,11 +9,10 @@ $mstest11 = "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\mst
 $msbuild  = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
 $msbuild4 = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
 
+Set-Alias notepad  "C:\Program Files (x86)\Notepad++\notepad++.exe"
 Set-Alias devenv   "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe"
 Set-Alias devenv10 "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe"
 Set-Alias devenv11 "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe"
 Set-Alias msbuild  "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
 Set-Alias mstest   "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\mstest.exe"
-
-Get-Module -ListAvailable | Import-Module
 
