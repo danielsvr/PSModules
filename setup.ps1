4dd19748f3767d5e7219ba7c1126caac9aaa1a8a
yochanan.miykael <yochanan.miykael@gmail.com>
2014-05-27 00:15:38 +0300

    trying to diferentiate from git clone and download

--- setup.ps1
+++ setup.ps1
@@ -3,45 +3,55 @@
 
 $profilepath = Split-Path $PROFILE -parent
 
-# temporary redirect
-$profilepath = ("{0}_tmp" -f $profilepath)
-# Write-Host $profilepath
-
-$tempPath = $env:TEMP
-
-$tempProfileZip = "$tempPath\PSProfile.zip"
-$tempProfileUnzip = "$tempPath\PSProfile"
-# Write-Host $tempProfileZip
-
-if(Test-Path $tempProfileZip) {
-  Write-Warning "$tempProfileZip exixts and it will be removed"
-  Remove-Item $tempProfileZip
-}
-
-# fetch profile
-Write-Host "Donwloading https://github.com/ogman/PSModules/archive/master.zip"
-Write-Host "         to $tempProfileZip"
-(New-Object System.Net.WebClient).`
-  DownloadFile("https://github.com/ogman/PSModules/archive/master.zip", `
-               $tempProfileZip)
-Write-Host "Done"
-
-Write-Host "Unziping $tempProfileZip"
-Write-Host "      to $tempProfileUnzip"
-
-
-Remove-Item -Recurse -Force $tempProfileUnzip -ErrorAction SilentlyContinue
-[System.IO.Compression.ZipFile]::`
-  ExtractToDirectory($tempProfileZip, $tempProfileUnzip)
-Write-Host "Done"
-
 if(Test-Path $profilepath) {
   Write-Warning "$profilepath exists and it will be deleted"
   Remove-Item -Recurse -Force $profilepath
 }
-Write-Host "Moving unziped content to $profilepath"
-Move-Item "$tempProfileUnzip\PSModules-master" $profilepath
-Write-Host "Done"
+
+Function DownloadZippedProfile {
+    # temporary redirect
+    # $profilepath = ("{0}_tmp" -f $profilepath)
+    # Write-Host $profilepath
+
+    $tempPath = $env:TEMP
+
+    $tempProfileZip = "$tempPath\PSProfile.zip"
+    $tempProfileUnzip = "$tempPath\PSProfile"
+    # Write-Host $tempProfileZip
+
+    if(Test-Path $tempProfileZip) {
+      Write-Warning "$tempProfileZip exixts and it will be removed"
+      Remove-Item $tempProfileZip
+    }
+
+    # fetch profile
+    Write-Host "Donwloading https://github.com/ogman/PSModules/archive/master.zip"
+    Write-Host "         to $tempProfileZip"
+    (New-Object System.Net.WebClient).`
+      DownloadFile("https://github.com/ogman/PSModules/archive/master.zip", `
+                  $tempProfileZip)
+    Write-Host "Done"
+
+    Write-Host "Unziping $tempProfileZip"
+    Write-Host "      to $tempProfileUnzip"
+
+
+    Remove-Item -Recurse -Force $tempProfileUnzip -ErrorAction SilentlyContinue
+    [System.IO.Compression.ZipFile]::`
+      ExtractToDirectory($tempProfileZip, $tempProfileUnzip)
+    Write-Host "Done"
+
+    Write-Host "Moving unziped content to $profilepath"
+    Move-Item "$tempProfileUnzip\PSModules-master" $profilepath
+    Write-Host "Done"
+}
+
+$gitExists = Get-Command git -ErrorAction SilentlyContinue
+if(-not ($gitExists -eq $null)) {
+  git clone https://github.com/ogman/PSModules.git $profilepath
+} else {
+  DownloadZippedProfile
+}
 
 if(-not $env:Path.tolower().Contains("chocolatey")) {
   iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
