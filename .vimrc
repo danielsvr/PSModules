7781f1a194425fe02645ec26dce42bbd7cdebc85
yochanan.miykael <yochanan.miykael@gmail.com>
2014-05-26 23:39:15 +0300

    adding my vim profile files

---
+++ .vimrc
@@ -0,0 +1,98 @@
+execute pathogen#infect()
+
+" Source the vimrc file after saving it
+autocmd bufwritepost .vimrc source $MYVIMRC
+
+set nocompatible
+
+filetype on
+filetype indent on
+filetype plugin on
+
+if has("gui_running")
+  " GUI is running or is about to start.
+  " Maximize gvim window.
+  set guifont=Lucida\ Console:h11
+  au GUIEnter * simalt ~x
+endif
+
+
+syntax enable
+set foldmethod=manual
+set ignorecase
+set hlsearch
+set autoindent
+set fileencoding=utf-8
+set encoding=utf-8
+set backspace=indent,eol,start
+set ts=2 sts=2 sw=2 expandtab
+
+set noeb vb t_vb= " disable belling sounds and visuals
+
+" mapped '\y ' for copying into system clipboard
+noremap <leader>y "+y 
+
+" mapped '\p ' for pasting from system clipboard
+noremap <leader>p "+p
+
+" mapped \TAB for buffer next command
+noremap <leader><tab> :bn<cr>
+" mapped \SHIFT TAB for buffer prev command
+noremap <leader><s-tab> :bp<cr>
+
+" mapped CTRL-N for new empty buffer
+noremap <c-N> :enew<cr>
+inoremap <c-N> <esc>:enew<cr>
+
+" mapped CRTL-W for closing current buffer
+noremap <c-W> :bd<cr>
+inoremap <c-W> <esc>:w<cr>:bd<cr>
+
+" mapped crtl s for save
+noremap  :w<cr>
+inoremap  <esc>:w<cr>I
+
+" mapped crtl shift s for save all
+noremap <s-> :wa<cr>
+inoremap <s-> <esc>:wa<cr>I
+
+set smartcase
+set gdefault
+set incsearch
+set showmatch
+noh
+
+" search hilight toggle
+noremap <leader>sh :set hlsearch!<cr>
+
+set winwidth=84
+set winheight=5
+set winminheight=5
+set winheight=999
+
+set nolist
+set listchars=tab:°>,eol:¶,trail:°
+set number
+set noswapfile
+set novisualbell
+set nocursorline
+set nowrap
+set linebreak
+"set background=dark
+colo desert
+
+"hi PreProc ctermfg=white
+"hi String ctermfg=cyan
+
+au FileType cs set omnifunc=syntaxcomplete#Complete
+au FileType cs set foldmethod=marker 
+au FileType cs set foldmarker={,} 
+au FileType cs set foldtext=substitute(getline(v:foldstart),'{.*','{...}',) 
+au FileType cs set foldlevelstart=-1 
+
+set virtualedit=onemore
+set shiftwidth=2
+
+set hidden
+
+" try omnisharp ... it seems nice
---
+++ setup.ps1
@@ -0,0 +1,66 @@
+[System.Reflection.Assembly]::`
+  LoadWithPartialName('System.IO.Compression.FileSystem') | Out-Null
+
+$profilepath = Split-Path $PROFILE -parent
+
+# temporary redirect
+$profilepath = ("{0}_tmp" -f $profilepath)
+# Write-Host $profilepath
+
+$tempPath = $env:TEMP
+
+$tempProfileZip = "$tempPath\PSProfile.zip"
+$tempProfileUnzip = "$tempPath\PSProfile"
+# Write-Host $tempProfileZip
+
+if(Test-Path $tempProfileZip) {
+  Write-Warning "$tempProfileZip exixts and it will be removed"
+  Remove-Item $tempProfileZip
+}
+
+# fetch profile
+Write-Host "Donwloading https://github.com/ogman/PSModules/archive/master.zip"
+Write-Host "         to $tempProfileZip"
+(New-Object System.Net.WebClient).`
+  DownloadFile("https://github.com/ogman/PSModules/archive/master.zip", `
+               $tempProfileZip)
+Write-Host "Done"
+
+Write-Host "Unziping $tempProfileZip"
+Write-Host "      to $tempProfileUnzip"
+
+
+Remove-Item -Recurse -Force $tempProfileUnzip -ErrorAction SilentlyContinue
+[System.IO.Compression.ZipFile]::`
+  ExtractToDirectory($tempProfileZip, $tempProfileUnzip)
+Write-Host "Done"
+
+if(Test-Path $profilepath) {
+  Write-Warning "$profilepath exists and it will be deleted"
+  Remove-Item -Recurse -Force $profilepath
+}
+Write-Host "Moving unziped content to $profilepath"
+Move-Item "$tempProfileUnzip\PSModules-master" $profilepath
+Write-Host "Done"
+
+if(-not $env:Path.tolower().Contains("chocolatey")) {
+  iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+  $env:PATH="{0};{1}\chocolatey\bin" -f $env:PATH,$env:SystemDrive
+}
+
+cinst vim
+
+Write-Host "Creating .vimrc hardlink"
+if(Test-Path "$env:USERPROFILE\.vimrc") {
+  Remove-Item -Force "$env:USERPROFILE\.vimrc"
+}
+cmd /c mklink /H $env:USERPROFILE\.vimrc $profilepath\.vimrc | Out-Null
+Write-Host "Done"
+
+Write-Host "Creating vimfiles junction"
+if(Test-Path "$env:USERPROFILE\vimfiles") {
+  Remove-Item -Recurse -Force "$env:USERPROFILE\vimfiles"
+}
+cmd /c mklink /J $env:USERPROFILE\vimfiles $profilepath\vimfiles
+Write-Host "Done"
+
