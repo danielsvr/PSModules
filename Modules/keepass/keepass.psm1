e90e8bb236c5143dcd128f2065f23424e12e0e06
Daniel Severin <severin.daniel@outlook.com>
2015-04-29 22:50:22 +0300

    fixing keepass

--- Modules/keepass/keepass.psm1
+++ Modules/keepass/keepass.psm1
@@ -1,7 +1,12 @@
+#
+##
+## opening keepass with a default db and a encrypted stored password
+##
+#
 Function Invoke-KeePass {
 param(
 [switch]$NoProfile,
-[string]$db = "C:\Users\IBM_ADMIN\OneDriveHome\OneDrive\lockInfo\MyKees.kdbx"
+[string]$db = "$($env:USERONEDRIVE)\lockInfo\MyKees.kdbx"
 )
 $keepass = Get-Command "keepass.exe" -ErrorAction Ignore
 if(-not $keepass) {
@@ -30,10 +35,46 @@ $securePw =(Get-Content -Path $kpass | ConvertTo-SecureString);
 $pw = $([System.Runtime.InteropServices.Marshal]::PtrToStringAuto(`
     [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePw)))
 
+if($env:KEEPASSDEFAULTDB -eq $null) {
+  Write-Warning "Failed to auto-detect db."
+  Write-Verbose "Environment vartiable KEEPASSDEFAULTDB is not set."
+
+  start "$($keepass.Path)"
+  return
+}
+
+if(Test-Path $env:USERONEDRIVE) {
+  $db = "$($env:USERONEDRIVE)\$($env:KEEPASSDEFAULTDB)"
+}
+
+if(-not (Test-Path $db)) {
+  Write-Verbose """$db"" not found."
+  $db = "$($env:USERPROFILE)\OneDriveHome\OneDrive\$($env:KEEPASSDEFAULTDB)"
+}
+
+if(-not (Test-Path $db)) {
+  Write-Verbose """$db"" not found."
+  $db = "$($env:USERPROFILE)\SkyDrive\$($env:KEEPASSDEFAULTDB)"
+}
+
+if(-not (Test-Path $db)) {
+  Write-Verbose """$db"" not found."
+  Write-Warning "Failed to auto-detect db."
+
+  start "$($keepass.Path)"
+  return
+}
+
 start "$($keepass.Path)" -ArgumentList ($db, "-pw:$pw")
 
 }
 
+
+#
+##
+## Saving an encrypted password in a profile file
+##
+#
 Function Set-KeePass {
 param(
 [string]$password,
