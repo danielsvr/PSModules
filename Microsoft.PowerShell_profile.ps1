bb3c2ab8ef073f37bbaeeef2f44040c8de59bb26
Daniel Severin <severin.daniel@outlook.com>
2016-04-26 16:46:48 +0300

    certificate stuff

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -69,7 +69,7 @@ Set-Alias devenv10 "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\
 Set-Alias devenv11 "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe"
 Set-Alias msbuild  "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
 Set-Alias mstest   "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\mstest.exe"
-Set-Alias procex   "C:\Chocolatey\lib\procexp.15.13\tools\procexp.exe"
+Set-Alias procex   "C:\ProgramData\chocolatey\bin\procexp.exe"
 Set-Alias remote   "mstsc"
 Set-Alias :q       ex
 Set-Alias ^Q       ex
--- Modules/windows-stuff/windows-stuff.psm1
+++ Modules/windows-stuff/windows-stuff.psm1
@@ -1,104 +1,104 @@
 function Set-AppCompatFlag {
-<# 
-       .SYNOPSIS 
-        Sets the Compatibility flags for an application. 
-       
-       .EXAMPLE 
+<#
+       .SYNOPSIS
+        Sets the Compatibility flags for an application.
+
+       .EXAMPLE
         Set-AppCompatFlag.ps1 -Path 'c:\windows\system32\WindowsPowerShell\v1.0\PowerShell_ISE.exe'
         This sets the RUNASADMIN flag for the ISE
-        
-       .EXAMPLE 
-        Set-AppCompatFlag.ps1 -Path 'c:\windows\system32\WindowsPowerShell\v1.0\PowerShell_ISE.exe' -ComputerName RemoteServer01 
+
+       .EXAMPLE
+        Set-AppCompatFlag.ps1 -Path 'c:\windows\system32\WindowsPowerShell\v1.0\PowerShell_ISE.exe' -ComputerName RemoteServer01
         This sets the RUNASADMIN flag for the ISE on RemoteServer01.
-        
+
        .PARAMETER  Path
         The full path to the executable to flag for compatibility.
-       
+
        .PARAMETER  ComputerName
         Computer to run the command against.  If this is a remote computer, the Remote Registry service needs to be running.  This defaults to the local machine.
-       
+
        .PARAMETER  Scope
         The scope to set the compatibility flag at.  This can be the CurrentUser or the LocalMachine level.  The default is CurrentUser.
-       
+
        .PARAMETER  Flag
         The compatibility flag to set.  Currently supports:
-            "RUNASADMIN", 
-            "WINSRV03SP1", 
+            "RUNASADMIN",
+            "WINSRV03SP1",
             "WINSRV08SP1",
-            "WINXPSP2", 
-            "WINXPSP3", 
-            "DISABLETHEMES", 
-            "640X480", 
-            "HIGHDPIAWARE", 
+            "WINXPSP2",
+            "WINXPSP3",
+            "DISABLETHEMES",
+            "640X480",
+            "HIGHDPIAWARE",
             "256COLOR",
-            "DISABLEDWM".  
+            "DISABLEDWM".
         The default is RUNASADMIN.
 
-       .NOTES 
+       .NOTES
         NAME: Set-AppCompatFlag
-        AUTHOR: Steven Murawski 
+        AUTHOR: Steven Murawski
         LASTEDIT: February 07, 2011 5:27:44 PM
-        KEYWORDS: 
-         
-        #Requires -Version 2.0 
-#> 
+        KEYWORDS:
+
+        #Requires -Version 2.0
+#>
     param(
         [Parameter(
             Mandatory=$true,
             ValueFromPipeline=$true,
             ValueFromPipelineByPropertyName=$true,
-            Position=0)] 
+            Position=0)]
         [alias('FullName','FullPath')]
-        [string] 
+        [string]
         $Path,
         [Parameter(
             ValueFromPipeline=$true,
             ValueFromPipelineByPropertyName=$true,
-            Position=1)] 
-        [string[]] 
+            Position=1)]
+        [string[]]
         $ComputerName,
         [Parameter(Position=2)]
         [string]
-        [ValidateSet("CurrentUser", "LocalMachine")] 
+        [ValidateSet("CurrentUser", "LocalMachine")]
         $Scope = "CurrentUser",
         [Parameter(Position=3)]
         [string]
-        [ValidateSet("RUNASADMIN", 
-            "WINSRV03SP1", 
+        [ValidateSet("RUNASADMIN",
+            "WINSRV03SP1",
             "WINSRV08SP1",
-            "WINXPSP2", 
-            "WINXPSP3", 
-            "DISABLETHEMES", 
-            "640X480", 
-            "HIGHDPIAWARE", 
+            "WINXPSP2",
+            "WINXPSP3",
+            "DISABLETHEMES",
+            "640X480",
+            "HIGHDPIAWARE",
             "256COLOR",
-            "DISABLEDWM")] 
+            "DISABLEDWM")]
         $Flag = "RUNASADMIN"
     )
-    
+
     process
     {
         if (($ComputerName -eq $null) -or ($ComputerName.count -lt 1))
         {
             $ComputerName = @($env:COMPUTERNAME)
         }
-        
+
         foreach ($Computer in $ComputerName)
         {
             try
             {
                 Write-Debug "Opening Remote Registry"
-                $reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($Scope, $Computer)   
-                                   
-                Write-Debug 'Reading SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\AppCompatFlags\\Layers'   
+                $reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($Scope, $Computer)
+
+                Write-Debug 'Reading SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\AppCompatFlags\\Layers'
                 $keys = $reg.OpenSubKey('SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\AppCompatFlags\\Layers', $true)
-                
-                
+
+
                 Write-Debug "Checking to see if $Path already has AppCompatFlags"
-                
+
                 $NotFlagged = $true
                 $NotSet = $true
-                
+
                 if ($keys.GetValueNames() -contains $Path)
                 {
                     Write-Debug "Checking to see if $Path already has $Flag set"
@@ -112,19 +112,19 @@ function Set-AppCompatFlag {
                         $CurrentValue = $keys.GetValue($Path)
                         Write-Debug "Adding $Flag for $path  to $Current on $Computer"
                         $NewFlag = "$CurrentValue $Flag"
-                        
+
                         Write-Debug "Setting $Flag for $path on $Computer"
                         $Keys.SetValue($path, $NewFlag)
                         Write-Verbose "Set $NewFlag for $path on $Computer"
                         $NotSet = $false
                     }
                 }
-                
+
                 if ($NotFlagged -and $NotSet)
                 {
                     Write-Debug "Setting $Flag for $path on $Computer"
                     $keys.SetValue("$path","$flag")
-                    
+
                     Write-Verbose "Set $Flag for $path on $Computer"
                 }
                 else
@@ -146,9 +146,87 @@ function proc(){
 #param(
 #  $action
 #)
-    
-  gwmi Win32_Process 
+
+  gwmi Win32_Process
   #-Filter "name = 'java.exe'" | select CommandLine,ProcessId | Format-List
 }
 
+function New-SelfSigned-Certificate(){
+param(
+    [string]$CertDomain = "DevCert",
+    [string]$RootCertName = 'DevRoot',
+    [string]$RootCertPassword = 'Dev123',
+    [string]$DevCertPassword = 'Dev123',
+    [string]$WPath = "$profilepath\.data\.certs",
+    [string]$MakecertPath = $null
+)
+
+    if((-not $MakecertPath) -or (-not (Test-Path $MakecertPath)))
+    {
+        $MakecertPath = "C:\Program Files (x86)\Windows Kits\10\bin\x64\"
+    }
+    if((-not $MakecertPath) -or (-not (Test-Path $MakecertPath)))
+    {
+        $MakecertPath = "C:\Program Files (x86)\Windows Kits\8.1\bin\x64\"
+    }
+    if((-not $MakecertPath) -or (-not (Test-Path $MakecertPath)))
+    {
+        $MakecertPath = "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\x64\"
+    }
+
+    $MakecertCommand = Get-Command "$MakecertPath\makecert.exe"
+    $Pvk2PfxCommand = Get-Command "$MakecertPath\pvk2pfx.exe"
+
+    if(-not (Test-Path $WPath)){
+        mkdir $WPath
+    }
+
+    if($MakecertCommand -and $Pvk2PfxCommand) {
+
+        $RootCertExists = $false;
+        $InstalledRootCert = Get-ChildItem Cert:\LocalMachine\Root `
+            | Where-Object { $_.Subject.Contains($RootCertName) } `
+            | ForEach-Object { `
+            $RootCertExists = $true `
+        };
+        # Write-Host "RootCertExists: $RootCertExists"
+
+        $RootCerFile = "$WPath\$RootCertName.cer";
+        $RootPvkFile = "$WPath\$RootCertName.pvk";
+        $RootPfxFile = "$WPath\$RootCertName.pfx";
+
+        $DevCerFile = "$WPath\$CertDomain.cer";
+        $DevPvkFile = "$WPath\$CertDomain.pvk";
+        $DevPfxFile = "$WPath\$CertDomain.pfx";
+
+        if(-not $RootCertExists){
+
+            if(Test-Path $RootCerFile) {
+                Remove-Item $RootCerFile;
+            }
+            if(Test-Path $RootPvkFile) {
+                Remove-Item $RootPvkFile;
+            }
+            if(Test-Path $RootPfxFile) {
+                Remove-Item $RootPfxFile;
+            }
+
+            # Write-Host "$MakecertCommand -n 'CN=$RootCertName' -r -pe -a sha512 -len 4096 -cy authority -sv $RootPvkFile $RootCerFile;"
+            & $MakecertCommand -sr LocalMachine -ss Root -n "CN=$RootCertName" -r -pe -a sha512 -len 4096 -cy authority -sv $RootPvkFile $RootCerFile;
+            & $Pvk2PfxCommand -pvk $RootPvkFile -spc $RootCerFile -pfx $RootPfxFile -po $RootCertPassword;
+        }
+
+        & $MakecertCommand -n "CN=$CertDomain" -iv $RootPvkFile -ic $RootCerFile -pe -a sha512 -len 4096 -b 01/01/2014 -e 01/01/2022 -sky exchange -eku 1.3.6.1.5.5.7.3.1 -sv $DevPvkFile $DevCerFile
+        & $Pvk2PfxCommand -pvk $DevPvkFile -spc $DevCerFile -pfx $DevPfxFile -po $DevCertPassword
+
+        Write-Warning 'Please be advised, the pfx password is not the one set in the dialogs. It is the password set via $DevCertPassword parameter'
+    }
+}
+
+
+Set-Alias newssc New-SelfSigned-Certificate
+
 Export-ModuleMember -Function Set-AppCompatFlag
+Export-ModuleMember -Function New-SelfSigned-Certificate
+
+Export-ModuleMember -Alias newssc
